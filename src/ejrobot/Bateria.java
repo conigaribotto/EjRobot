0d6d67c6bedde81a66b3fbb424f7c45101f28924
Coni <coti.garibotto@gmail.com>
2023-08-11 18:00:24 -0300

    Bateria

--- src/ejrobot/Bateria.java
+++ src/ejrobot/Bateria.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejrobot;
 
-/**
- *
- * @author Usuario
- */
 public class Bateria {
+    private String tipoBateria;
+    private int carga;
     
+    
+    public Bateria(){
+        this.carga=1000;
+        
+    }
+
+    public String getTipoBateria() {
+        return tipoBateria;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setTipoBateria(String tipoBateria) {
+        this.tipoBateria = tipoBateria;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+   
 }
+
