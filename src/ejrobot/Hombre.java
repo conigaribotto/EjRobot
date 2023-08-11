7b564392ff437243d143ffaf21577fd99eaf72a4
JonathanCristianEscofe <cristianjonathan218@gmail.com>
2023-08-11 17:57:56 -0300

    create Hombre

---
+++ src/ejrobot/Hombre.java
@@ -0,0 +1,23 @@
+
+package ejrobot;
+
+
+public class Hombre {
+    
+    private String genero;
+    private int edad;
+    private String nombre;
+    private String apellido;
+
+    public Hombre(String genero, int edad, String nombre, String apellido) {
+        this.genero = genero;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public void JugarConRobot() {
+        
+    }    
+    
+}
